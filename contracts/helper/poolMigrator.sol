4bd0c2f8341fd0efbd73221d186938d17cfd5cd0
Mac&Cheese <81648569+macNcheeseFinance@users.noreply.github.com>
2022-06-02 11:41:23 +0200

    Update poolMigrator.sol

--- contracts/helper/poolMigrator.sol
+++ contracts/helper/poolMigrator.sol
@@ -1,8 +1,10 @@
-// SPDX-License-Identifier: None
+// SPDX-License-Identifier: BUSL-1.1
 
-import "./libs/standard/IERC20.sol";
-import "./libs/standard/Address.sol";
-import "./libs/standard/SafeERC20.sol";
+pragma solidity 0.8.0;
+
+import "../libs/standard/IERC20.sol";
+import "../libs/standard/Address.sol";
+import "../libs/standard/SafeERC20.sol";
 
 interface IToken {
 	function swapOldForNew(uint256 _amount) external returns(bool);
@@ -17,7 +19,7 @@ contract XVMCmigrator {
     using SafeERC20 for IERC20;
 	
 	IERC20 public immutable oldToken = IERC20(0x6d0c966c8A09e354Df9C48b446A474CE3343D912);
-	IERC20 public immutable token;
+	address public immutable token = 0x84F71F85202E84d27b42199a2cE8d65CeF1EA189;
 	
 	address public immutable oldPool;
 	address public immutable newPool;
@@ -28,9 +30,8 @@ contract XVMCmigrator {
 		}
 	}
 	
-   constructor(IERC20 _XVMC, address _oldPool, address _newPool) {
-        token = _XVMC;
-		oldToken.safeApprove(address(token), type(uint256).max); //to exchange(swap) old for new token
+   constructor(address _oldPool, address _newPool) {
+		IERC20(0x6d0c966c8A09e354Df9C48b446A474CE3343D912).safeApprove(0x84F71F85202E84d27b42199a2cE8d65CeF1EA189, type(uint256).max); //to exchange(swap) old for new token
 		oldPool = _oldPool;
 		newPool = _newPool;
     }
@@ -40,7 +41,7 @@ contract XVMCmigrator {
 		
 		oldToken.safeTransferFrom(oldPool, address(this), _amount);
 		
-		require(token.swapOldForNew(_amount));
+		require(IToken(token).swapOldForNew(_amount));
 		
 		IacPool(newPool).hopDeposit((_amount*1000), _recipient, _lastDepositedTime, _mandatoryTimeToServe);
 	}
