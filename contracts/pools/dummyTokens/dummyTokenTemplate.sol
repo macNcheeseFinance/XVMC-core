4c937eca1c222a448451c67ad391446bf686e1f6
Mac&Cheese <81648569+macNcheeseFinance@users.noreply.github.com>
2022-06-05 14:24:50 +0200

    Update dummyTokenTemplate.sol

--- contracts/pools/dummyTokens/dummyTokenTemplate.sol
+++ contracts/pools/dummyTokens/dummyTokenTemplate.sol
@@ -3,28 +3,22 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IToken {
     function governor() external view returns (address);
 }
 
-contract XVMCtrackerCDP is ERC20, ERC20Burnable, Ownable {
-	address public immutable XVMCtoken = 0x84F71F85202E84d27b42199a2cE8d65CeF1EA189;
+contract XVMCtrackerCDP is ERC20, ERC20Burnable {
+	address public immutable XVMCtoken = ;
 
     constructor(string memory _forDuration) ERC20("Dummy Token", _forDuration) {}
+    
+    modifier onlyOwner() {
+        require(msg.sender == IToken(XVMCtoken).governor(), "only governor allowed");
+        _;
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-
-	/*
-	* transfers ownership to Governor
-	* XVMCtoken is owned by Masterchef
-	* Masterchef is owned by Governor
-	*/
-    function updateOwnerToGovernor() external {
-        _transferOwnership(IToken(XVMCtoken).governor());
-    }
-
 }
