36e4ffc53dfb2fa7b7e673b65ffc1dcc3ae48bb1
Mac&Cheese <81648569+macNcheeseFinance@users.noreply.github.com>
2022-06-05 14:21:49 +0200

    Update dummyToken.sol

--- contracts/oldChef/dummyToken.sol
+++ contracts/oldChef/dummyToken.sol
@@ -3,22 +3,26 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IMasterchef {
     function owner() external view returns (address);
 }
 
-contract XVMColdMasterchefRewards is ERC20, ERC20Burnable, Ownable {
+interface IToken {
+	function governor() external view returns (address);
+}
+
+contract XVMColdMasterchefRewards is ERC20, ERC20Burnable {
 	address public immutable oldMasterchef = 0x9BD741F077241b594EBdD745945B577d59C8768e;
 
     constructor() ERC20("Collecting Rewards", "Previous XVMC") {}
-
+	
+    modifier onlyOwner() {
+        require(msg.sender == IToken(xvmc).governor(), "admin: wut?");
+        _;
+    }
+	
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
-    }
-
-    function updateOwner() external {
-        _transferOwnership(IMasterchef(oldMasterchef).owner());
     }
 }
